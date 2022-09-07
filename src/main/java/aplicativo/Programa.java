93a07a75bb0f18a1082625749ea837574a472c71
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2022-09-06 22:37:33 -0300

    Corrigindo projeto aluno

--- src/main/java/aplicativo/Programa.java
+++ src/main/java/aplicativo/Programa.java
@@ -2,15 +2,31 @@ package aplicativo;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
         Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
         Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(p1);
+        entityManager.persist(p2);
+        entityManager.persist(p3);
+
+        entityManager.getTransaction().commit();
+
+
+        System.out.println("pronto");
+
 
     }
 }
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,16 +1,22 @@
 package dominio;
 
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
 
+    public  Pessoa() {
 
+    }
 
     public Pessoa(Integer id, String nome, String email) {
         this.id = id;
